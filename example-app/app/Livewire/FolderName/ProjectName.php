1d3792aaf7e57914e269dbb62af6ce6c51e8fa68
Kobsie <kobericafrente2001@gmail.com>
2024-09-09 19:18:13 +0800

    Initial commit

---
+++ example-app/app/Livewire/FolderName/ProjectName.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\FolderName;
+
+use Livewire\Component;
+
+class ProjectName extends Component
+{
+    public function render()
+    {
+        return view('livewire.folder-name.project-name');
+    }
+}
---
+++ ideas/resources/views/terms.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.layout')
+
+@section('content')
+<h1>Terms</h1>
+<div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores necessitatibus nisi fuga assumenda voluptatibus est
+    quas, earum error velit, tenetur eum dolorum, vel doloremque?
+</div>
+@endsection
